b3cc12006a764ab7854d3202a5beed215a166626
d-flytech <desirevanbrummen@hotmail.com>
2024-08-04 16:48:20 +0200

    added transition in javacript

--- script.js
+++ script.js
@@ -4,20 +4,30 @@ window.addEventListener('DOMContentLoaded', () => {
     const min_el = document.querySelector('.min');
     const sec_el = document.querySelector('.sec');
 
-    function rotateClockHands () {
-        
-        const now = new Date ();
-        const seconds = now.getSeconds ();
-        const minutes = now.getMinutes ();
-        const hours = now.getHours ();
+    let prevSecRotation = 0;
+
+    function rotateClockHands() {
+
+        const now = new Date();
+        const seconds = now.getSeconds();
+        const minutes = now.getMinutes();
+        const hours = now.getHours();
 
         const secRotation = seconds * 6;
         const minRotation = minutes * 6 + seconds * 0.1;
         const hourRotation = hours * 30 + minutes * 0.5;
 
+        if (secRotation > prevSecRotation) {
+            sec_el.style.transition = 'transform 0.6s steps(60, end)';
+        } else {
+            sec_el.style.transition = 'none';
+        }
+
         sec_el.style.transform = `rotate(${secRotation}deg)`;
         min_el.style.transform = `rotate(${minRotation}deg)`;
         hour_el.style.transform = `rotate(${hourRotation}deg)`;
+
+        prevSecRotation = secRotation;
 
     }
 
